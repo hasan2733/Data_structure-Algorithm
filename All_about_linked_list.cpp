7420ce1f3cb6757d1c98029cddfda00aed60b91a
Abid Hasan <hasan.tng33@gmail.com>
2024-12-06 16:31:04 +0600

    Update and rename insertion_of_linked_list.cpp to All_about_linked_list.cpp

--- All_about_linked_list.cpp
+++ All_about_linked_list.cpp
@@ -62,6 +62,54 @@ void insertat_Pos(node *&head,int data,int pos)
   n->next=temp->next;
   temp->next=n;
 }
+void count_num(node *&head)
+{
+  if(head==NULL)
+  {
+    cout<<"The linkedlist is empty"<<endl;
+  }
+  node* temp=head;
+  int cnt=0;
+  while(temp!=NULL)
+  {
+    temp=temp->next;
+    cnt++;
+  }
+  cout<<"The length of linkedlist is : "<<cnt<<endl;
+}
+
+void delete_an_element(node *&head,int data)
+{
+  if(head==NULL)
+  {
+    return;
+  }
+  if(head->data==data)
+  {
+    node *todelete=head;
+    head=head->next;
+    free(todelete);
+    return;
+  }
+  node *temp=head;
+  while(temp->next->data!=data)
+  {
+    temp=temp->next;
+  }
+  node *todelete=temp->next;
+  temp->next=temp->next->next;
+  free(todelete);
+}
+
+void update_data(node *&head,int data,int data2)
+{
+  node* temp=head;
+  while(temp->data!=data)
+  {
+    temp=temp->next;
+  }
+  temp->data=data2;
+}
 int main() 
 {
     node *head=NULL;
@@ -72,4 +120,14 @@ int main()
     insertAt_tail(head,720);
     insertat_Pos(head,66,2);
     display(head);
+    cout<<endl;
+    count_num(head);
+    delete_an_element(head,5);
+    display(head);
+    cout<<endl;
+    count_num(head);
+    update_data(head,720,72);
+    display(head);
+    cout<<endl;
+    count_num(head);
 }
